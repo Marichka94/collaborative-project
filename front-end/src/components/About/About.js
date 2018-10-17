59a0a3acf4e66931b5c14d948dddf8923d1c4273
Marichka Tsiuriak <marichka.ts94@gmail.com>
2018-10-17 14:29:35 -0400

    Create About cmpnt

---
+++ front-end/src/components/About/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './About.css'
+import image from './image/about.jpg'
+
+const About = () => {
+  return (
+    <div className="about">
+      <h1 className="mt-5">About Us</h1>
+      <p className="">After noticing a gap in the market for small business strategy, the founders of Atruity created an online product that serves as a tool kit for small businesses.</p>
+      <img src={image} alt="pretty-photo" />
+    </div>
+  )
+}
+
+export default About
