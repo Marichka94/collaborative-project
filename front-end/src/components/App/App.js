1a5d005116fa64333092f526caf65fc4e2667079
“GRACE <GRACE.CHEUNG@YALE.EDU>
2018-10-17 11:45:39 -0400

    link frontend and backend

--- front-end/src/components/App/App.js
+++ front-end/src/components/App/App.js
@@ -3,20 +3,47 @@ import {Route, Link, Switch} from 'react-router-dom'
 import './App.css';
 import Homepage from '../Homepage/Homepage'
 import Footer from '../Footer/Footer'
+import Goals from '../Goals/Goals'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      goals: []
+    }
+  }
+
+  componentDidMount() {
+    this.getGoals()
+  }
+
+  getGoals = () => {
+    fetch('http://localhost:3001/api/goals')
+      .then(response => response.json())
+      .then(goals =>
+        this.setState({
+          goals: goals
+        })
+      )
+  }
+
   render() {
     return (
       <div className="App">
         <Homepage />
         <Footer />
-      </div>
-      <div>
+      <main>
         <Switch>
-          <Route to="/"/>
+         <Route
+              path="/goals"
+              render={props => {
+                return <Goals {...props} />
+              }}
+            />
           <Route to="/"/>
           <Route to="/"/>
         </Switch>
+        </main>
       </div>
     )
   }
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from '.components/App/App'
-import { BrowserRouter as Router } from 'react-router-dom'
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App/App';
+import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router} from 'react-router-dom'
 
 ReactDOM.render(
   <Router>
