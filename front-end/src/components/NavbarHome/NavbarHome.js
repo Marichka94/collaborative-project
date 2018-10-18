fe93b594cb2ff6ad7d596cdbdd8d794f2ec17057
Marichka Tsiuriak <marichka.ts94@gmail.com>
2018-10-18 01:53:33 -0400

    Add link style

--- front-end/src/components/NavbarHome/NavbarHome.js
+++ front-end/src/components/NavbarHome/NavbarHome.js
@@ -7,15 +7,15 @@ const NavbarHome = () => {
     <div>
       <nav className="p-2 d-flex justify-content-between align-items-center">
         <h1 className='brand pl-3'>Atruity</h1>
-        <NavLink className="text-light pr-3" to='/'>Log In</NavLink>
+        <NavLink className="pr-3 link" to='/'>Log In</NavLink>
       </nav>
       <div className="text-center crop">
         <img className="image" src="https://files.slack.com/files-pri/TDGDKH7T7-FDGLWDYE9/desk-3139127_640.jpg" alt='banner'/>
       </div>
       <nav className="p-2 d-flex justify-content-center align-items-center">
-        <a className="p-2 mr-5 text-light"  href="#about">About</a>
-        <a className="p-2 text-light" href="#product">Product</a>
-        <a className="p-2 ml-5 text-light" href="#reviews">Reviews</a>
+        <a className="p-2 mr-5 link"  href="#about">About</a>
+        <a className="p-2 link" href="#product">Product</a>
+        <a className="p-2 ml-5 link" href="#reviews">Reviews</a>
       </nav>
     </div>
   )
