9340e5613379662e9bad434ec935e4d646e93c1a
Marichka Tsiuriak <marichka.ts94@gmail.com>
2018-10-18 10:58:39 -0400

    Finish style for Sidebar component

--- front-end/src/components/Userpage/Userpage.js
+++ front-end/src/components/Userpage/Userpage.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
-import Footer from "../Footer/Footer";
-import NavbarUser from "../NavbarUser/NavbarUser";
+import Footer from "../Footer/Footer"
+import NavbarUser from "../NavbarUser/NavbarUser"
+import Sidebar from "../Sidebar/Sidebar"
+import './Userpage.css'
 
 class Userpage extends Component {
   render() {
     return(
       <div>
         <NavbarUser />
-        <h1>Userpage</h1>
+        <div className='d-flex'>
+          <Sidebar />
+          <div className='content text-center'>
+            <h1>Userpage</h1>
+          </div>
+        </div>
         <Footer />
       </div>
     )
