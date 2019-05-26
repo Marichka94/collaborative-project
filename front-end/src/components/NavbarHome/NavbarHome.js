e51cb1d3c4ce9cee20ad4ed08e0fae5d12db3383
Marichka Tsiuriak <marichka.offen@gmail.com>
2019-05-26 15:36:18 -0400

    Quick fix

--- front-end/src/components/NavbarHome/NavbarHome.js
+++ front-end/src/components/NavbarHome/NavbarHome.js
@@ -1,26 +1,37 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import './NavbarHome.css'
-import Signup from '../SignupForm/Signup'
-import Info from '../SignupForm/Info'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./NavbarHome.css";
+import Signup from "../SignupForm/Signup";
+import Info from "../SignupForm/Info";
+import image from "./image/desk.jpg";
 
 const NavbarHome = () => {
   return (
     <div>
       <nav className="p-2 d-flex justify-content-between align-items-center">
-        <NavLink to="/" className='brand pl-3'>Atruity</NavLink>
-        <NavLink className="pr-3 link" to='/userpage'>Log In</NavLink>
+        <NavLink to="/" className="brand pl-3">
+          Atruity
+        </NavLink>
+        <NavLink className="pr-3 link" to="/userpage">
+          Log In
+        </NavLink>
       </nav>
       <div className="text-center crop">
-        <img className="image" src="https://files.slack.com/files-pri/TDGDKH7T7-FDGLWDYE9/desk-3139127_640.jpg" alt='banner'/>
+        <img className="image" src={image} alt="banner" />
       </div>
       <nav className="p-2 d-flex justify-content-center align-items-center">
-        <a className="p-2 mr-5 link"  href="#about">About</a>
-        <a className="p-2 link" href="#product">Product</a>
-        <a className="p-2 ml-5 link" href="#reviews">Reviews</a>
+        <a className="p-2 mr-5 link" href="#about">
+          About
+        </a>
+        <a className="p-2 link" href="#product">
+          Product
+        </a>
+        <a className="p-2 ml-5 link" href="#reviews">
+          Reviews
+        </a>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default NavbarHome
+export default NavbarHome;
